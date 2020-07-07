65425e04b74cd2536a8d8e8bd0830a110471c89b
benstanfish <benstanfish@hotmail.com>
2020-07-06 22:24:02 -0700

    Update v0.1.1

--- NavGator/Form1.Designer.cs
+++ NavGator/Form1.Designer.cs
@@ -233,7 +233,7 @@
             this.Controls.Add(this.buttonOriginal);
             this.Name = "Form1";
             this.ShowIcon = false;
-            this.Text = "NavGator v0.0";
+            this.Text = "NavGator v0.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
